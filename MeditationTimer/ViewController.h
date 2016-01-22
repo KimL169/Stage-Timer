b16accb32f08f88675b47699683eb364658b6ebb
Kim Landeweer <krelandeweer@gmail.com>
2016-01-22 16:10:42 +0100

    Initial Commit

---
+++ MeditationTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeditationTimer
+//
+//  Created by Kim on 22/01/16.
+//  Copyright © 2016 Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MeditationTimer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MeditationTimer
+//
+//  Created by Kim on 22/01/16.
+//  Copyright © 2016 Kim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
